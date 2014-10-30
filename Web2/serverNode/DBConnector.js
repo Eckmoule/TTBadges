dc72bf7860015fc9ec706e0bed07426173501943
Nahoy <yohan.bejannin@gmail.com>
2014-10-30 08:31:38 +0100

    Think about an AngularJS / Php rest server config

---
+++ Web2/serverNode/DBConnector.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql')
+	config = require('/config/config.js')
+
+var connection = mysql.createConnection({
+	  host     : config.CONNECTION_STRING.host,
+	  user     : config.CONNECTION_STRING.user,
+	  password : config.CONNECTION_STRING.password,
+	  database : config.CONNECTION_STRING.database,
+	});
+
+connection.connect(function(err) {
+	  if (err) {
+	    console.error('Error connecting: ' + err.stack);
+	    return;
+	  }
+	  console.log('Connected as id %d', connection.threadId);
+	});
+
+module.exports.Connection = connection;
+
